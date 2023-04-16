09bc7b7bf6ebb4d305c324742f95a96ca5e0d69e
jStrider <ju.renaud974@gmail.com>
2023-04-16 17:15:51 +0200

    feat: added an automatic upgrade at entrypoint

--- jrw-discord-bot.sh
+++ jrw-discord-bot.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 source /data/env
+python3 -m pip install --upgrade -i https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple/ jrw-discord-bot
 python3 -m jrw-discord-bot
